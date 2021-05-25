28b46ae9ee0919a1ad7947496a0510fd0d6293fd
justbeaver97 <73840274+justbeaver97@users.noreply.github.com>
2021-05-25 22:20:20 +0900

    0525(working on main showing pic)

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
+from werkzeug.utils import secure_filename
+import os
+import datetime 
 
 app = Flask(__name__)
 app.secret_key = 'software_engineering'
@@ -7,8 +11,11 @@ app.secret_key = 'software_engineering'
 cluster = MongoClient(
   "mongodb+srv://YeHyunSuh:Tjdmdgka55!@cluster0.qbm6b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 database = cluster["software_engineering"]
-taskDB = database["tasks"]
+diaryDB = database["diary"]
 userDB = database["user"]
+
+app.config['MONGO_URI']='mongodb+srv://YeHyunSuh:Tjdmdgka55!@cluster0.qbm6b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 #Route
 @app.route('/')
@@ -22,10 +29,10 @@ def login():
     return render_template('login.html')
   elif request.method == 'POST':
     id = request.form['id']
-    password = request.form['password']
+    password = request.form['password']   
 
     #Find the data that matches the id and password
-    user = userDB.find_one(
+    user = userDB.find_one(  
       {'id': id },
       {'password': password } 
     )
@@ -57,11 +64,11 @@ def signup():
     name = request.form['name']
     email = request.form['email']
     
-    addUser = userDB.find_one(
+    addUser = userDB.find_one( 
       {'id': id }
     )
     if addUser is None:
-      addUser = userDB.insert({
+      userDB.insert({
         'id': request.form['id'],
         'password': request.form['password'],
         'name': request.form['name'],
@@ -75,9 +82,80 @@ def signup():
 
 @app.route('/main')
 def main():
+  i = 0
   if 'id' in session:
     id = session['id']
-    return render_template('main.html')
+  diary = mongo.db.users
+  output = []
+  count = 0
+  path_arr = []
+  for item in diary.find():
+    output.append({
+      'title': item['title'],
+      'description': item['description'],
+      'profile_image_name': item['profile_image_name'],
+      'date': item['date']
+    })
+    str = output[count]['profile_image_name']
+    path='file/'+str
+    path_arr.append(path)
+    count += 1
+  print(path_arr)
+  return render_template('main.html', output = output, path = path_arr)
+
+
+@app.route('/post', methods=['POST', 'GET'])
+def post():
+  if request.method == 'GET':
+    return render_template('post.html')
+  elif request.method == 'POST':
+    title = request.form['title']
+    description = request.form['description']
+    f = request.files['file']
+
+    if not os.path.exists("./data"):
+      os.makedirs('./data')
+    filename = secure_filename(f.filename)
+
+    mongo.save_file(f.filename, f)
+    mongo.db.users.insert_one({
+      'title': request.form['title'],
+      'description': request.form['description'],      
+      'profile_image_name': f.filename,
+      'date': datetime.datetime.now()
+    })
+    # diaryDB.insert_one({
+    #   'title': request.form['title'],
+    #   'description': request.form['description'],
+    #   'filename': f.filename
+    # })
+    return redirect(url_for('main'))
+
+# @app.route('/file/<filename>')
+# def file(filename):
+#   return mongo.send_file(filename)
+
+# @app.route('/profile/<title>')
+# def profile(title):
+#   user = mongo.db.users.find_one_or_404({'title' : title})
+#   return f'''
+#     <h1>{title}</h1>
+#     <img src="{url_for('file',filename=user['profile_image_name'])}">
+#   '''
+
+@app.route('/edit', methods=['POST', 'GET'])
+def edit():
+  if request.method == 'GET':
+    return render_template('edit.html')
+  elif request.method == 'POST':
+    title = request.form['title']
+    description = request.form['description']
+
+    addDiary = diaryDB.insert({
+      'title': request.form['title'],
+      'description': request.form['description']
+    })
+    return redirect(url_for('main'))
 
 
 if __name__ == "__main__":
